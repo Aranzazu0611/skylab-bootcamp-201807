d9a4d3b4bb48fce40eac9d7f65c3e4d6e5e4fb5f
manuelbarzi <manuelbarzi@gmail.com>
2018-09-05 12:04:51 +0200

    update testing

--- staff/aranzazu-perdomo/wall-book/src/logic/index.spec.js
+++ staff/aranzazu-perdomo/wall-book/src/logic/index.spec.js
@@ -17,33 +17,40 @@ describe('logic', () => {
     const comment = 'fantastic'
 
     true && describe('validate fields', () => {
-
         it('should succeed on correct value', () => {
             expect(() => logicWallbook._validateEmail(email)).not.to.throw()
             expect(() => logicWallbook._validateStringField('password', password)).not.to.throw()
             expect(() => logicWallbook._validateStringField('name', name)).not.to.throw()
         })
+
         it('should fail on undefined value', () => {
             expect(() => logicWallbook._validateEmail(undefined)).to.throw(`invalid email`)
         })
+
         it('should fail on empty value', () => {
             expect(() => logicWallbook._validateEmail('')).to.throw(`invalid email`)
         })
+
         it('should fail on blank value', () => {
             expect(() => logicWallbook._validateEmail('    ')).to.throw(`invalid email`)
         })
+
         it('should fail on numeric value', () => {
             expect(() => logicWallbook._validateEmail('12345')).to.throw(`invalid email`)
         })
+
         it('should fail on undefined value', () => {
             expect(() => logicWallbook._validateStringField('name', undefined)).to.throw(`invalid name`)
         })
+
         it('should fail on numeric value', () => {
             expect(() => logicWallbook._validateStringField('name', 12345)).to.throw(`invalid name`)
         })
+
         it('should fail on empty value', () => {
             expect(() => logicWallbook._validateStringField('name', '')).to.throw(`invalid name`)
         })
+
         it('should fail on blank value', () => {
             expect(() => logicWallbook._validateStringField('name', '    ')).to.throw(`invalid name`)
         })
@@ -52,74 +59,81 @@ describe('logic', () => {
     true && describe('register user', () => {
         it('should register correctly', () =>
             logicWallbook.register(email, name, password)
+                .catch(({message}) => expect(message).to.be.undefined)
                 .then(res => expect(res).to.be.true)
         )
+
         it('should fail on undefined email', () =>
             logicWallbook.register(undefined, name, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on empty email', () =>
             logicWallbook.register('', name, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on blank email', () =>
             logicWallbook.register('     ', name, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on numeric email', () =>
             logicWallbook.register('     ', name, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on undefined name', () =>
             logicWallbook.register(email, undefined, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid name'))
         )
+
         it('should fail on empty name', () =>
             logicWallbook.register(email, '', password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid name'))
         )
+
         it('should fail on blank name', () =>
             logicWallbook.register(email, '    ', password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid name'))
         )
+
         it('should fail on numeric name', () =>
             logicWallbook.register(email, 12345, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid name'))
         )
+
         it('should fail on undefined password', () =>
             logicWallbook.register(email, name, undefined)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
+        
         it('should fail on empty password', () =>
             logicWallbook.register(email, name, '')
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
+
         it('should fail on blank password', () =>
             logicWallbook.register(email, name, '    ')
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
-
     })
 
     true && describe('authenticate user', () => {
         it('should authenticate correctly', () =>
             logicWallbook.authenticate(email, password)
-                .catch(({message}) => {
-                    debugger
-                    
-                    expect(message).to.be.undefined
-                })
+                .catch(({message}) => expect(message).to.be.undefined)
                 .then(({ message, token, user }) => {
                     expect(token).to.exist
                     expect(user).to.exist
@@ -127,6 +141,7 @@ describe('logic', () => {
                     expect(message).to.equal(message)
                 })
         )
+
         it('should fail on trying to authenticate with undefined email', () =>
             logicWallbook.authenticate(undefined, password)
                 .catch(({ message }) => message)
@@ -138,80 +153,87 @@ describe('logic', () => {
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on trying to authenticate with a blank email', () =>
             logicWallbook.authenticate('   ', password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on trying to authenticate with a numeric email', () =>
             logicWallbook.authenticate(12345, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on trying to authenticate with undefined password', () =>
             logicWallbook.authenticate(email, undefined)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
+
         it('should fail on trying to authenticate with empty password', () =>
             logicWallbook.authenticate(email, '')
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
+
         it('should fail on trying to authenticate with a blank password', () =>
             logicWallbook.authenticate(email, '    ')
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
-
     })
 
     true && describe('unregister user', () => {
-
         it('should unregister correctly', () =>
             logicWallbook.unregister(email, password)
-                .catch(({message}) => message)
+                .catch(({message}) => expect(message).to.be.undefined)
                 .then(res => expect(res).to.be.true)
         )
+
         it('should fail on trying to unregister with undefined email', () =>
             logicWallbook.unregister(undefined, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on trying to unregister with empty email', () =>
             logicWallbook.unregister('', password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on trying to unregister with a numeric email', () =>
             logicWallbook.unregister(12345, password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on trying to unregister with a blank email', () =>
             logicWallbook.unregister('   ', password)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid email'))
         )
+
         it('should fail on trying to unregister with undefined password', () =>
             logicWallbook.unregister(email, undefined)
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
+
         it('should fail on trying to unregister with an empty password', () =>
             logicWallbook.unregister(email, '')
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
+
         it('should fail on trying to unregister with an blank password', () =>
             logicWallbook.unregister(email, '     ')
                 .catch(({ message }) => message)
                 .then(message => expect(message).to.equal('invalid password'))
         )
-
     })
-
-
 })
 
 
